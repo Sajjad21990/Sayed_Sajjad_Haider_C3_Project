2480a5a17de3199a192317c69d6d2af737f9fb7c
Sajjad21990 <sajjadhaider21990@gmail.com>
2022-01-04 09:45:22 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantServiceTest.java
+++ src/test/java/RestaurantServiceTest.java
@@ -4,6 +4,9 @@ import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 class RestaurantServiceTest {
 
@@ -15,7 +18,9 @@ class RestaurantServiceTest {
         LocalTime openingTime = LocalTime.parse("10:30:00");
         LocalTime closingTime = LocalTime.parse("22:00:00");
 
-         restaurant = service.addRestaurant("Amelie's cafe","Chennai", openingTime, closingTime);
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai", openingTime, closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
     }
 
 
@@ -36,9 +41,6 @@ class RestaurantServiceTest {
     @Test
     public void remove_restaurant_should_reduce_list_of_restaurants_size_by_1() throws restaurantNotFoundException {
        
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-
         int initialNumberOfRestaurants = service.getRestaurants().size();
         service.removeRestaurant("Amelie's cafe");
         assertEquals(initialNumberOfRestaurants-1, service.getRestaurants().size());
@@ -46,22 +48,33 @@ class RestaurantServiceTest {
 
     @Test
     public void removing_restaurant_that_does_not_exist_should_throw_exception() throws restaurantNotFoundException {
-       
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-
         assertThrows(restaurantNotFoundException.class,()->service.removeRestaurant("Pantry d'or"));
     }
 
     @Test
     public void add_restaurant_should_increase_list_of_restaurants_size_by_1(){
 
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-
         int initialNumberOfRestaurants = service.getRestaurants().size();
         service.addRestaurant("Pumpkin Tales","Chennai",LocalTime.parse("12:00:00"),LocalTime.parse("23:00:00"));
         assertEquals(initialNumberOfRestaurants + 1,service.getRestaurants().size());
     }
     //<<<<<<<<<<<<<<<<<<<<ADMIN: ADDING & REMOVING RESTAURANTS>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    // TODO
+    // calling calculate price method with list of items should return total amount
+    // calling calculate price method with empty list should return 0
+
+    @Test
+    public void calulate_total_price_should_return_total_price_of_items_selected(){
+
+       List<String> itemsSelected = new ArrayList<String>();
+       itemsSelected.add("Sweet corn soup");
+       itemsSelected.add("Vegetable lasagne");
+
+       int totalAmount = service.calculateTotalAmount(itemsSelected);
+            
+
+    }
+
+
 }
